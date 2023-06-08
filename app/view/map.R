5983c4b4ff144aeea5a724ae304aa523db4a7fb0
Tanesya <tanesya.t@gmail.com>
2023-06-08 16:30:54 +0700

    chore: modify map size

--- app/view/map.R
+++ app/view/map.R
@@ -1,7 +1,14 @@
 box::use(
     dplyr[`%>%`, between, filter, group_by, summarise, right_join, ungroup],
     lubridate[ymd],
-    echarts4r[echarts4rOutput, e_charts, e_color, e_map, e_visual_map, renderEcharts4r],
+    echarts4r[
+        echarts4rOutput,
+        e_charts,
+        e_color,
+        e_map,
+        e_visual_map,
+        renderEcharts4r
+    ],
     shiny[moduleServer, NS, plotOutput, reactive, renderPlot]
 )
 
@@ -24,7 +31,7 @@ server  <- function(id, data) {
 
             dat %>%
               e_charts(country)  %>%
-              e_map(revenue)  %>%
+              e_map(revenue, center = list(0, 0))  %>%
               e_visual_map(revenue, inRange = list(color = c("#0971ef", "#97c5fa"))) %>%
               e_color(background = "#2c2e38")
         })
